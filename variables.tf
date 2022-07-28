4d9a3e1cfe5fb214bfaf6b948b6b1ae1beb30291
smitaniusha <107972889+smitaniusha@users.noreply.github.com>
2022-07-28 21:24:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "us-east-2"
+  default     = "ap-southeast-1"
 }
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "myJune2021Key"
+  default     =  "singapore-key"
 }
 
 variable "instance_type" {
