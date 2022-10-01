3ef53eebd872403f36d383ba5cbeb07315756efa
smitaniusha <107972889+smitaniusha@users.noreply.github.com>
2022-10-01 15:44:05 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "ap-southeast-1"
+  default     = "ap-south-1"
 }
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "singapore-key"
+  default     =  "smita2022new"
 }
 
 variable "instance_type" {
@@ -25,5 +25,5 @@ variable "tag_name" {
 
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0b9064170e32bde34"
+  default     = "ami-05c8ca4485f8b138a"
 }
